e01fc1fbc24a41133298de8ab536b0989a8d335d
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-08 00:25:36 +0600

    compare between two pair

---
+++ comparism2.cpp
@@ -0,0 +1,11 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<int,int>p1,p2;
+    p1={10,3};
+    p2={3,78};
+    if(p1<p2) cout<<"YES"<<endl;
+    else cout<< "NO"<<endl;
+}
