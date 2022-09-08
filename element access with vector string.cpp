69f0439cb60feda333254766904a624a9c637776
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-08 09:48:52 +0600

    print the element with vector in pair

---
+++ element access with vector string.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<string,vector<string>>p;
+    p.first="Md Nasim Hossen";
+    p.second={"Nirob","Robiul","Amir Hamza","Fozor"};
+    cout<<p.first<<endl;
+    for(auto u:p.second)
+    {
+        cout<<u<<endl;
+    }
+
+}
