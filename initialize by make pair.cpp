6bbf7883bc308788a33c034bc79e5a4212fc9cc9
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-08 09:55:00 +0600

    initialize the pair by using make pair

---
+++ initialize by make pair.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<int,int>p;
+    p=make_pair (2,3);
+    cout<<p.first<< " "<<p.second<<endl;
+
+
+
+    pair<string,string>r;
+    r=make_pair("Nasim","ayesha");
+    cout<<r.first<<" "<<r.second<<endl;
+
+
+    pair<float,float>t;
+    t=make_pair(12.32,256.214);
+    cout<<t.first<<" "<<t.second<<endl;
+
+
+}
