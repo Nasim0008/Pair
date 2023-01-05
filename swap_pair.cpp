852b79e60801c2e5fe6a2d256e47750cae9060f2
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-01-05 18:18:09 +0600

    swap the value of the pair

--- swap_pair.cpp
+++ swap_pair.cpp
@@ -3,8 +3,10 @@ using namespace std;
 int main()
 {
     pair<string,int>p1;
-    pair<string,int>p2={"Md Nasim Hossen",20};
-    p1.swap(p2);
+    pair<string , int>p2;
+    p1=make_pair("nasim",10);
+    p2=make_pair("ayesha",20);
+    swap(p1,p2);
     cout<<p1.first<< " "<<p1.second<<endl;
-
+    cout<<p2.first<< " "<<p2.second<<endl;
 }
